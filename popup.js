d28025a4a9004fc5e112a5b5283d404948beec5c
Taylor Shin <t2shin@ucsd.edu>
2017-04-03 15:05:05 -0700

    sort tracked sites from most hit to least

--- popup.js
+++ popup.js
@@ -1,18 +1,25 @@
 // Given an array of URLs, build a DOM list of these URLs in the browser action popup.
 function buildPopupDom() {
-  var popupDiv = document.getElementById('visited_div');
-  var ol = popupDiv.appendChild(document.createElement('ol'));
+    var popupDiv = document.getElementById('visited_div');
+    var ol = popupDiv.appendChild(document.createElement('ol'));
 
-  chrome.storage.local.get(null, function(items) {
-      for(key in items) {
-          var li = ol.appendChild(document.createElement('li'));
-          var a = li.appendChild(document.createElement('a'));
-          var h = li.appendChild(document.createElement('h4'));
-          a.href = key;
-          a.appendChild(document.createTextNode(key));
-          h.appendChild(document.createTextNode(items[key]));
-      }
-  })
+    chrome.storage.local.get(null, function(items) {
+        var keys = [];
+        for(key in items) {
+            keys.push(key);
+        }
+        keys.sort(function(a, b){ return items[b] - items[a] });
+        keys.forEach(function(key) {
+            var li = ol.appendChild(document.createElement('li'));
+            var a = li.appendChild(document.createElement('a'));
+            var p = li.appendChild(document.createElement('p'));
+            var h = li.appendChild(document.createElement('h4'));
+            a.href = key;
+            a.appendChild(document.createTextNode(key));
+            p.appendChild(document.createTextNode('URL Hits:'));
+            h.appendChild(document.createTextNode(items[key]));
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
